c66dc9d9e673befdfcfb549c884657d9709462a0
Solomon Hawk <solomon.hawk@viget.com>
2021-12-01 00:51:14 -0500

    [sfh] Clean up, factor out shared fn

--- solomonhawk/src/day1.rs
+++ solomonhawk/src/day1.rs
@@ -5,32 +5,29 @@ pub fn input_generator(input: &str) -> Vec<usize> {
 
 #[aoc(day1, part1)]
 pub fn part1(nums: &[usize]) -> usize {
-    nums.iter()
-        .fold((0, 0), |(n, l), &m| {
-            if l > 0 && m > l {
-                return (n + 1, m);
-            }
-
-            return (n, m);
-        })
-        .0
+    count_increasing_seqs(nums.iter().cloned())
 }
 
 #[aoc(day1, part2)]
 pub fn part2(nums: &[usize]) -> usize {
-    nums[0..nums.len() - 2]
+    let iter = nums[0..nums.len() - 2]
         .iter()
         .enumerate()
         .map(|(i, _)| &nums[i..i + 3])
-        .map(|ns| ns.iter().sum::<usize>())
-        .fold((0, 0), |(n, l), m| {
-            if l > 0 && m > l {
-                return (n + 1, m);
-            }
+        .map(|ns| ns.iter().sum::<usize>());
 
-            return (n, m);
-        })
-        .0
+    count_increasing_seqs(iter)
+}
+
+pub fn count_increasing_seqs(nums: impl Iterator<Item = usize>) -> usize {
+    nums.fold((0, 0), |(n, l), m| {
+        if l > 0 && m > l {
+            return (n + 1, m);
+        }
+
+        return (n, m);
+    })
+    .0
 }
 
 #[cfg(test)]
@@ -42,6 +39,14 @@ mod tests {
         assert_eq!(
             input_generator("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"),
             [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        );
+    }
+
+    #[test]
+    fn increasing_seqs() {
+        assert_eq!(
+            count_increasing_seqs([199, 200, 208, 200].iter().cloned()),
+            2
         );
     }
 
