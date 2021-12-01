b1893a295136c56ee1e4f0b8ca8b33c547b294a3
Joe Jackson <cpmhjoe@gmail.com>
2021-12-01 14:43:17 -0500

    Day01 Part 1 and Part 2

--- joe/day-01/Day01.hs
+++ joe/day-01/Day01.hs
@@ -1,4 +1,23 @@
 module Day01 where
 
+import Data.List (tails)
+
+windows :: Int -> [Int] -> [[Int]]
+windows x = filter (\ys -> length ys == x) . map (take x) . tails
+
+solve1 :: [Int] -> Int
+solve1 = length . filter (\ys ->  ys !! 1 > head ys) . windows 2
+
+solve2 :: [Int] -> Int
+solve2 = solve1 . map sum . windows 3
+
 main :: IO ()
-main = putStrLn "Hello, Haskell"
+main = do
+  input <- readFile "day-01/input.txt"
+  let xs = map read $ lines input
+
+  putStrLn "Part 1"
+  print $ solve1 xs
+
+  putStrLn "Part 2"
+  print $ solve2 xs
