d7dea3d238d8149f97f87bbcb435fa67708647a3
Solomon Hawk <solomon.hawk@viget.com>
2021-12-01 00:33:50 -0500

    [sfh] Day 1, Part 1 & 2

--- solomonhawk/src/day1.rs
+++ solomonhawk/src/day1.rs
@@ -1,23 +1,36 @@
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<usize> {
-    input
-        .trim()
-        .split("\n")
-        .map(|n| n.parse().unwrap())
-        .collect()
+    input.split("\n").map(|n| n.parse().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
 pub fn part1(nums: &[usize]) -> usize {
-    for n in nums.iter() {
-        for m in nums.iter() {
-            if n + m == 2020 {
-                return n * m;
+    nums.iter()
+        .fold((0, 0), |(n, l), &m| {
+            if l > 0 && m > l {
+                return (n + 1, m);
             }
-        }
-    }
 
-    unreachable!()
+            return (n, m);
+        })
+        .0
+}
+
+#[aoc(day1, part2)]
+pub fn part2(nums: &[usize]) -> usize {
+    nums[0..nums.len() - 2]
+        .iter()
+        .enumerate()
+        .map(|(i, _)| &nums[i..i + 3])
+        .map(|ns| ns.iter().sum::<usize>())
+        .fold((0, 0), |(n, l), m| {
+            if l > 0 && m > l {
+                return (n + 1, m);
+            }
+
+            return (n, m);
+        })
+        .0
 }
 
 #[cfg(test)]
@@ -25,7 +38,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn input() {
+        assert_eq!(
+            input_generator("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"),
+            [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        );
+    }
+
+    #[test]
     fn sample1() {
-        assert_eq!(part1(&[1000, 1020]), 1020000);
+        assert_eq!(
+            part1(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            7
+        );
+    }
+
+    #[test]
+    fn sample2() {
+        assert_eq!(
+            part2(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            5
+        );
     }
 }
