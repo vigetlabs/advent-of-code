e0d077f5294471bc1138ebc8084ba5bb7e2b8d6b
DanielSBrown <danny.brown@viget.com>
2021-12-02 09:17:52 -0500

    Add day 2 solutions

--- dannybrown/src/helpers.py
+++ dannybrown/src/helpers.py
@@ -1,6 +1,13 @@
-def read_puzzle_input(file_name):
+def read_integer_puzzle_input(file_name):
   ret = []
   with open(file_name) as f:
     for line in f:
       ret.append(int(line))
+  return ret
+
+def read_string_puzzle_input(file_name):
+  ret = []
+  with open(file_name) as f:
+    for line in f:
+      ret.append(line)
   return ret
