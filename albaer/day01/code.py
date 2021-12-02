5cff994161e55da05e74164fbf5cddcb9d4fc0d7
Annie Kiley <annie.kiley@viget.com>
2021-12-02 11:27:48 -0500

    Refactor to use more methods

--- albaer/day01/code.py
+++ albaer/day01/code.py
@@ -3,15 +3,22 @@ from functools import reduce
 input_file_path = "input.txt"
 output_file_path = "solution.txt"
 
+def write_solution(solution):
+    with open(output_file_path, "w") as output_file:
+        output_file.write(str(solution))
+
+def read_input_lines():
+    with open(input_file_path, "r") as input_file:
+        return input_file.read().splitlines()
+
+input_lines = read_input_lines()
+
 def check_increase(total, index):
     previous_measurement = int(input_lines[index - 1])
     current_measurement = int(input_lines[index])
     increase = previous_measurement < current_measurement
     return total + 1 if increase else total
 
-with open(input_file_path, "r") as file:
-    input_lines = file.read().splitlines()
-    total_result = reduce(check_increase, range(len(input_lines)))
+total_result = reduce(check_increase, range(len(input_lines)))
 
-with open(output_file_path, "w") as solution:
-    solution.write(str(total_result))
+write_solution(total_result)
