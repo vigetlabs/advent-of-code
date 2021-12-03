418696040a72c167c6b518cffc9b303bdc72f7bf
Annie Kiley <annie.kiley@viget.com>
2021-12-03 13:34:41 -0600

    Working solution to part 1 of day 3

--- albaer/day03/code.py
+++ albaer/day03/code.py
@@ -15,6 +15,8 @@ def read_input_lines():
 
 input_lines = read_input_lines()
 
+# Part 1
+
 def bin_str_to_int(bin_str):
     return int(bin_str, 2)
 
@@ -28,21 +30,34 @@ def most_common_bits(lst):
     number_of_bits = len(lst[0])
     return [str(most_common_bit_at(lst, i)) for i in range(number_of_bits)]
 
+def least_common_bits(lst):
+    most_common = most_common_bits(lst)
+    return reverse_bits(most_common)
+
 def bits_to_bin_str(bits):
     return "".join(bits)
 
-def calculate_gamma(lst):
-    bits = most_common_bits(lst)
+def reverse_bits(lst):
+    return ["1" if i == "0" else "0" for i in lst]
+
+def bits_to_int(bits):
     bin_str = bits_to_bin_str(bits)
     return bin_str_to_int(bin_str)
 
-gamma = calculate_gamma(input_lines)
+def calculate_gamma(lst):
+    bits = most_common_bits(lst)
+    return bits_to_int(bits)
 
-pprint(gamma)
+def calculate_epsilon(lst):
+    bits = least_common_bits(lst)
+    return bits_to_int(bits)
 
-# Part 1
+def calculate_power_consumption(lst):
+    gamma = calculate_gamma(lst)
+    epsilon = calculate_epsilon(lst)
+    return gamma * epsilon
 
-part_1_result = 0
+part_1_result = calculate_power_consumption(input_lines)
 
 # Part 2
 
