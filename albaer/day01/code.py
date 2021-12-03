abe21c93d71ab7264812f2e914817d65a6c91c07
Annie Kiley <annie.kiley@viget.com>
2021-12-02 23:03:20 -0500

    Complete part 2 of day 1

--- albaer/day01/code.py
+++ albaer/day01/code.py
@@ -19,8 +19,18 @@ def read_input_lines():
 def consecutive_groups(lst, n):
     return [lst[i:i+n] for i in range(len(lst)-n+1)]
 
+# Part 1
+
 inputs_as_integers = [int(i) for i in read_input_lines()]
 pairs = consecutive_groups(inputs_as_integers, 2)
-total_result = sum([1 for pair in pairs if pair[0] < pair[1]])
+part_1_result = sum([1 for pair in pairs if pair[0] < pair[1]])
 
-write_solution(total_result)
+# Part 2
+
+triplets = consecutive_groups(inputs_as_integers, 3)
+triplet_sums = [sum(triplet) for triplet in triplets]
+pairs_of_sums = consecutive_groups(triplet_sums, 2)
+part_2_result = sum([1 for pair in pairs_of_sums if pair[0] < pair[1]])
+
+solution = str(part_1_result) + "\n" + str(part_2_result)
+write_solution(solution)
