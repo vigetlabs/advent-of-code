069abf6aad1f0e8f8d5c58052092135480f57a32
Annie Kiley <annie.kiley@viget.com>
2021-12-02 22:42:10 -0500

    Add and use consecutive_groups method with list comp

--- albaer/day01/code.py
+++ albaer/day01/code.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from pprint import pprint
 
 input_file_path = "input.txt"
 output_file_path = "solution.txt"
@@ -13,17 +14,18 @@ def read_input_lines():
     with open(input_file_path, "r") as input_file:
         return input_file.read().splitlines()
 
+# Sort of like ruby's each_cons without the block option
+# consecutive_groups([1, 2, 3, 4], 2) returns [[1, 2], [2, 3], [3, 4]]
+def consecutive_groups(lst, n):
+    return [lst[i:i+n] for i in range(len(lst)-n+1)]
+
 input_lines = read_input_lines()
 
-def check_increase(total, index):
-    previous_measurement = int(input_lines[index - 1])
-    current_measurement = int(input_lines[index])
-    increase = previous_measurement < current_measurement
+def check_increase(total, pair):
+    increase = int(pair[0]) < int(pair[1])
     return total + 1 if increase else total
 
-total_result = reduce(check_increase, range(len(input_lines)))
+total_result = reduce(check_increase, consecutive_groups(input_lines, 2), 0)
 
 write_solution(total_result)
 
-# TODO: Part 2
-# TODO: Use list comprehensions for funsies
