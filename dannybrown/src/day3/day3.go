120632dab9ab2d4312c562bfc845d5a28685b6ba
DanielSBrown <danny.brown@viget.com>
2021-12-03 11:54:04 -0500

    Add day 2

--- dannybrown/src/day3/day3.go
+++ dannybrown/src/day3/day3.go
@@ -14,9 +14,12 @@ func check(e error) {
 	}
 }
 
-func rateGetter(bitsList []string, direction bool) int64 {
+func getBitFrequencies(bitsList []string) []map[string]int {
+	// Returns a list of maps, each map representing the counts of 0s and 1s for that index in the list
+	// i.e. [{"0": 0, "1": 3}, {"0": 2, "1": 1}, {"0": 0, "1": 3}] for a list of ["101", "111", "101"]
+
 	bitStringLength := len(bitsList[0])
-	var gammaBitList []string
+	var bitFrequenciesList []map[string]int
 
 	for i := 0; i < bitStringLength; i++ {
 		currentBitMap := make(map[string]int)
@@ -26,7 +29,64 @@ func rateGetter(bitsList []string, direction bool) int64 {
 		for _, bitString := range bitsList {
 			currentBitMap[string(bitString[i])]++
 		}
+		bitFrequenciesList = append(bitFrequenciesList, currentBitMap)
+	}
+	return bitFrequenciesList
+}
 
+
+func determineCommonBit(bitFrequency map[string]int, isMost bool) string {
+	if isMost {
+		if bitFrequency["1"] >= bitFrequency["0"]  {
+			return "1"
+		} else {
+			return "0"
+		}
+	} else {
+		if bitFrequency["1"] >= bitFrequency["0"]  {
+			return "0"
+		} else {
+			return "1"
+		}
+	}
+}
+
+
+
+func lifeSupportRating(bitsList []string, direction bool) int64 {
+	bitFrequenciesList := getBitFrequencies(bitsList)
+
+	bitsListCopy := make([]string, len(bitsList))
+	copy(bitsListCopy, bitsList)
+
+	var newBitsList []string
+
+	for i := range bitFrequenciesList {
+		if len(bitsListCopy) == 1 {
+			break
+		}
+		newBitFrequenciesList := getBitFrequencies(bitsListCopy)
+		currentBitMap := newBitFrequenciesList[i]
+
+		commonBit := determineCommonBit(currentBitMap, direction)
+		for _, bitString := range bitsListCopy {
+			if string(bitString[i]) == commonBit {
+				newBitsList = append(newBitsList, bitString)
+			}
+		}
+		bitsListCopy = newBitsList
+		newBitsList = []string{}
+	}
+	binaryRep, _ := strconv.ParseInt(bitsListCopy[0], 2, 64) // Thank you golang, very cool! :)
+	return binaryRep
+}
+
+
+func rateGetter(bitsList []string, direction bool) int64 {
+	var gammaBitList []string
+	bitFrequenciesList := getBitFrequencies(bitsList)
+
+	for _, currentBitMap := range bitFrequenciesList {
 		if direction { // switch on whether it's gamma (true) or epsilon (false)
 			if currentBitMap["0"] > currentBitMap["1"] {
 				gammaBitList = append(gammaBitList, "0")
@@ -42,7 +102,7 @@ func rateGetter(bitsList []string, direction bool) int64 {
 		}
 	}
 	joinedGammaBitList := strings.Join(gammaBitList, "")
-	binaryRep, _ := strconv.ParseInt(joinedGammaBitList, 2, 64)
+	binaryRep, _ := strconv.ParseInt(joinedGammaBitList, 2, 64) // Thank you golang, very cool! :)
 
 	return binaryRep
 }
@@ -54,8 +114,10 @@ func day3_part1(binaryInput []string) int64 {
 	return gamma*epsilon
 }
 
-func day3_part2(instructions []string) (int, int) {
-	return 0, 0
+func day3_part2(instructions []string) int64 {
+	oxygen := lifeSupportRating(instructions, true)
+	co2 := lifeSupportRating(instructions, false)
+	return oxygen*co2
 }
 
 func main() {
@@ -71,4 +133,5 @@ func main() {
 	}
 
 	fmt.Println(day3_part1(txtlines)) // Part 1
+	fmt.Println(day3_part2(txtlines)) // Part 2
 }
