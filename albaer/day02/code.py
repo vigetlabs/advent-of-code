c356987331ca4d1c8b24bee1d4e896925d17f2c9
Annie Kiley <annie.kiley@viget.com>
2021-12-03 10:33:10 -0500

    Moving transformations into list comprehensions

--- albaer/day02/code.py
+++ albaer/day02/code.py
@@ -17,21 +17,22 @@ input_lines = read_input_lines()
 
 # Part 1
 
+string_changes = [line.split() for line in input_lines]
+changes = [[change[0], int(change[1])] for change in string_changes]
+
 position = [0,0]
 
 def handle_change(change, depth=0, horizontal_position=0):
-    direction, distance = change.split()
-    pprint("direction: " + direction)
-    pprint("distance: " + distance)
+    direction, distance = change
     match direction:
         case "down":
-            return [depth + int(distance), horizontal_position]
+            return [depth + distance, horizontal_position]
         case "up":
-            return[depth - int(distance), horizontal_position]
+            return[depth - distance, horizontal_position]
         case "forward":
-            return [depth, horizontal_position + int(distance)]
+            return [depth, horizontal_position + distance]
 
-for change in input_lines:
+for change in changes:
   position = handle_change(change, position[0], position[1])
   pprint(position)
 
@@ -42,26 +43,26 @@ part_1_result = position[0] * position[1]
 position_and_aim = [0,0,0]
 
 def handle_aim_change(change, depth=0, horizontal_position=0, aim=0):
-    direction, distance = change.split()
-    pprint("direction: " + direction)
-    pprint("distance: " + distance)
+    direction, distance = change
     match direction:
         case "down":
-            return [depth, horizontal_position, aim + int(distance)]
+            return [depth, horizontal_position, aim + distance]
         case "up":
-            return [depth, horizontal_position, aim - int(distance)]
+            return [depth, horizontal_position, aim - distance]
         case "forward":
-            return [depth + aim * int(distance), horizontal_position + int(distance), aim]
+            return [depth + aim * distance, horizontal_position + distance, aim]
 
-for change in input_lines:
+for change in changes:
   position_and_aim = handle_aim_change(change, position_and_aim[0], position_and_aim[1], position_and_aim[2])
   pprint(position_and_aim)
 
 part_2_result = position_and_aim[0] * position_and_aim[1]
 pprint(part_2_result)
 
+# Write solution
+
 solution = str(part_1_result) + "\n" + str(part_2_result)
 write_solution(solution)
 
 # TODO: Refactor things used every day into shareable code
-# (reading input anf writing the solution)
+# (reading input and writing the solution)
