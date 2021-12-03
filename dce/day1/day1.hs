547667ae5e4565f4f20ea38e9d04b6896cfc3525
David Eisinger <david.eisinger@gmail.com>
2021-12-03 10:02:10 -0500

    [dce] Refactor day1 solution slightly

--- dce/day1/day1.hs
+++ dce/day1/day1.hs
@@ -1,19 +1,13 @@
 import System.IO
 
 part1 :: (Int, Int) -> Int -> (Int, Int)
-part1 cp depth =
-   let count = fst cp in
-   let prev = snd cp in
-
+part1 (count, prev) depth =
    if depth > prev
        then (count + 1, depth)
    else (count, depth)
 
 part2 :: (Int, [Int]) -> Int -> (Int, [Int])
-part2 cp depth =
-   let count = fst cp in
-   let prev = snd cp in
-
+part2 (count, prev) depth =
    if length prev < 3
       then (count, [depth] ++ prev)
    else
@@ -28,8 +22,8 @@ main = do
     input <- readFile "./input.txt"
     let depths = map read (lines input)
 
-    let result = foldl part1 (0, 10000) depths
-    putStrLn ("Part 1: " ++ show (fst result))
+    let (count, _) = foldl part1 (0, 10000) depths
+    putStrLn ("Part 1: " ++ show count)
 
-    let result = foldl part2 (0, []) depths
-    putStrLn ("Part 2: " ++ show (fst result))
+    let (count, _) = foldl part2 (0, []) depths
+    putStrLn ("Part 2: " ++ show count)
