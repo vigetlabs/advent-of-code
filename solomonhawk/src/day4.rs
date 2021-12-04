df9a0d2535059648d3a5c17c480d1d415218a293
Solomon Hawk <solomon.hawk@viget.com>
2021-12-04 14:46:13 -0500

    [sfh] Day 4, Part 2

--- solomonhawk/src/day4.rs
+++ solomonhawk/src/day4.rs
@@ -5,13 +5,15 @@ pub struct Bingo {
     pick_index: usize,
     picks: Vec<usize>,
     boards: Vec<Board>,
-    winner: Option<Board>,
+    winners: Vec<Board>,
     dimension: usize,
 }
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Board {
     spaces: Vec<Space>,
+    winner: bool,
+    last_pick: Option<usize>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -23,8 +25,8 @@ pub struct Space {
 }
 
 impl Bingo {
-    fn play(&mut self) -> &mut Bingo {
-        while self.winner.is_none() {
+    fn play_until_winner(&mut self) -> &mut Bingo {
+        while self.winners.len() == 0 {
             if self.pick_index == self.picks.len() {
                 println!("Ran out of picks!");
                 break;
@@ -38,25 +40,43 @@ impl Bingo {
                 // println!("------------");
             }
 
-            self.check_winner();
+            self.check_winners(pick);
 
-            if self.winner.is_none() {
-                self.pick_index += 1;
-            }
+            self.pick_index += 1;
         }
 
         self
     }
 
-    fn check_winner(&mut self) {
-        if let Some(_) = &self.winner {
-            return;
+    fn play_all_picks(&mut self) -> &mut Bingo {
+        while self.pick_index < self.picks.len() {
+            let pick = self.picks[self.pick_index];
+
+            for board in &mut self.boards {
+                mark_space(board, pick);
+            }
+
+            self.check_winners(pick);
+
+            self.pick_index += 1;
         }
 
-        for board in &self.boards {
+        self
+    }
+
+    fn check_winners(&mut self, pick: usize) {
+        for board in &mut self.boards {
+            if board.winner {
+                continue;
+            }
+
             for i in 0..self.dimension {
                 if check_row_win(board, i) || check_col_win(board, i) {
-                    self.winner = Some(board.clone());
+                    board.winner = true;
+                    board.last_pick = Some(pick);
+
+                    self.winners.push(board.clone());
+
                     break;
                 }
             }
@@ -154,7 +174,11 @@ pub fn input_generator(input: &str) -> Bingo {
                         })
                 })
                 .collect();
-            Board { spaces }
+            Board {
+                spaces,
+                winner: false,
+                last_pick: None,
+            }
         })
         .collect();
 
@@ -165,22 +189,34 @@ pub fn input_generator(input: &str) -> Bingo {
 pub fn part1(bingo: &Bingo) -> usize {
     let mut bingo = bingo.clone();
 
-    bingo.play();
+    bingo.play_until_winner();
 
-    if let Some(winner) = &bingo.winner {
-        sum_umarked_spaces(winner) * bingo.picks[bingo.pick_index]
+    if let Some(winner) = &bingo.winners.first() {
+        sum_unmarked_spaces(winner) * winner.last_pick.expect("Winning board missing last_pick")
     } else {
         println!("A winner was not found!");
         0
     }
 }
 
-// #[aoc(day4, part2)]
-// pub fn part2(bingo: &Bingo) -> usize {
-//     0
-// }
+#[aoc(day4, part2)]
+pub fn part2(bingo: &Bingo) -> usize {
+    let mut bingo = bingo.clone();
 
-fn sum_umarked_spaces(board: &Board) -> usize {
+    bingo.play_all_picks();
+
+    if let Some(last_winner) = &bingo.winners.last() {
+        sum_unmarked_spaces(last_winner)
+            * last_winner
+                .last_pick
+                .expect("Winning board missing last_pick")
+    } else {
+        println!("A winner was not found!");
+        0
+    }
+}
+
+fn sum_unmarked_spaces(board: &Board) -> usize {
     board
         .spaces
         .iter()
@@ -189,15 +225,47 @@ fn sum_umarked_spaces(board: &Board) -> usize {
         .sum()
 }
 
+// TODO(shawk): I should write some tests...
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn input() {
-    //     assert_eq!(
-    //         input_generator("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"),
-    //         []
-    //     );
-    // }
+    #[test]
+    fn input() {
+        let bingo = input_generator(
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+        22 13 17 11  0
+         8  2 23  4 24
+        21  9 14 16  7
+         6 10  3 18  5
+         1 12 20 15 19
+
+         3 15  0  2 22
+         9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+
+        14 21 17 24  4
+        10 16 15  9 19
+        18  8 23 26 20
+        22 11 13  6  5
+         2  0 12  3  7",
+        );
+
+        assert_eq!(
+            bingo.picks,
+            [
+                7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8,
+                19, 3, 26, 1
+            ]
+        );
+
+        assert_eq!(bingo.boards.len(), 3);
+
+        assert_eq!(bingo.boards[0].spaces.len(), 25);
+        assert_eq!(bingo.boards[1].spaces.len(), 25);
+        assert_eq!(bingo.boards[2].spaces.len(), 25);
+    }
 }
