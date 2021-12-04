af949882bfdefcac7a3c2eab6ce29cf0dd7c92ef
Annie Kiley <annie.kiley@viget.com>
2021-12-03 21:15:32 -0600

    Working solution to part 2 of day 3

--- albaer/day03/code.py
+++ albaer/day03/code.py
@@ -14,6 +14,7 @@ def read_input_lines():
         return input_file.read().splitlines()
 
 input_lines = read_input_lines()
+number_of_bits = len(input_lines[0])
 
 # Part 1
 
@@ -27,7 +28,6 @@ def most_common_bit_at(lst, n):
     return 1 if ones >= zeroes else 0
 
 def most_common_bits(lst):
-    number_of_bits = len(lst[0])
     return [str(most_common_bit_at(lst, i)) for i in range(number_of_bits)]
 
 def least_common_bits(lst):
@@ -61,7 +61,58 @@ part_1_result = calculate_power_consumption(input_lines)
 
 # Part 2
 
-part_2_result = 0
+# Oxygen Generator Rating
+
+def filter_by_most_common_bit_at(lst, n):
+    most_common_n_bit = most_common_bit_at(lst, n)
+    return [i for i in lst if i[n] == str(most_common_n_bit)]
+
+def find_ox_gen_rating(lst, index):
+    if len(lst) == 1:
+        return lst[0]
+    elif index > number_of_bits - 1:
+        return "ERROR"
+    else:
+        new_lst = filter_by_most_common_bit_at(lst, index)
+        return find_ox_gen_rating(new_lst, index + 1)
+
+def calculate_ox_gen_rating(lst, index):
+    bin_str = find_ox_gen_rating(input_lines, 0)
+    return bin_str_to_int(bin_str)
+
+# C02 Scrubber Rating
+
+def least_common_bit_at(lst, n):
+    bits_at_position = [int(i[n]) for i in lst]
+    ones = sum(bits_at_position)
+    zeroes = len(lst) - ones
+    return 1 if ones < zeroes else 0
+
+def filter_by_least_common_bit_at(lst, n):
+    least_common_n_bit = least_common_bit_at(lst, n)
+    return [i for i in lst if i[n] == str(least_common_n_bit)]
+
+def find_co2_scrubber_rating(lst, index):
+    if len(lst) == 1:
+        return lst[0]
+    elif index > number_of_bits - 1:
+        return "ERROR"
+    else:
+        new_lst = filter_by_least_common_bit_at(lst, index)
+        return find_co2_scrubber_rating(new_lst, index + 1)
+
+def calculate_co2_scrubber_rating(lst, index):
+    bin_str = find_co2_scrubber_rating(input_lines, 0)
+    return bin_str_to_int(bin_str)
+
+co2_scrubber_rating = calculate_co2_scrubber_rating(input_lines, 0)
+
+def calculate_life_support_rating(lst):
+    ox_gen_rating = calculate_ox_gen_rating(lst, 0)
+    co2_scrubber_rating = calculate_co2_scrubber_rating(lst, 0)
+    return ox_gen_rating * co2_scrubber_rating
+
+part_2_result = calculate_life_support_rating(input_lines)
 
 # Write solution
 
