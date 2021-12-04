27b1bd6d006209229faa90b516b1593a243017c0
Eli Fatsi <eli.fatsi@viget.com>
2021-12-04 04:13:49 -0500

    [ef] Refactor 3.1 to pull out helper methods for part 2

--- efatsi/day_3/main.go
+++ efatsi/day_3/main.go
@@ -7,18 +7,24 @@ import (
   "strconv"
 )
 
+// const readings_count = 1000
+// const reading_length = 12
+const readings_count = 12
+const reading_length = 5
+
 func main() {
-  data, err := os.ReadFile("input.txt")
+  // data, err := os.ReadFile("input.txt")
+  data, err := os.ReadFile("example.txt")
   if err != nil {
     panic(err)
   }
   trimmed_data := strings.Trim(string(data), "\n ")
   readings := strings.Split(trimmed_data, "\n")
 
-  // Assemble data into arrays of positioned bits
-  const readings_count = 1000
-  const reading_length = 12
-  var array_of_arrays [reading_length][readings_count]int
+  // Assemble data into a slice of arrays of positioned bits
+  // Slice: useful for making functions that operate on the dataset able to take
+  //   slices of dynamic length
+  array_of_arrays := make([][readings_count]int, reading_length)
 
   for i := 0; i < readings_count; i++ {
     for j := 0; j < reading_length; j++ {
@@ -26,29 +32,52 @@ func main() {
     }
   }
 
-  // Assemble strings of most/least common bits
-  var most_common_bits string
-  var least_common_bits string
-  for j := 0; j < reading_length; j++ {
+  solvePartOne(array_of_arrays)
+}
+
+func solvePartOne(array_of_arrays [][readings_count]int) {
+  // Part 1 solution
+  most_common_bits, least_common_bits := getMostCommon(array_of_arrays)
+  most_common_bit_string := arrayToString(most_common_bits)
+  least_common_bit_string := arrayToString(least_common_bits)
+
+  gamma, err := strconv.ParseInt(most_common_bit_string, 2, 64)
+  epsilon, err := strconv.ParseInt(least_common_bit_string, 2, 64)
+  check(err)
+
+  fmt.Println(gamma * epsilon)
+}
+
+func getMostCommon(array_of_arrays [][readings_count]int) ([]int, []int) {
+  most_common_bits := make([]int, len(array_of_arrays))
+  least_common_bits := make([]int, len(array_of_arrays))
+
+  for j := 0; j < len(array_of_arrays); j++ {
     sum := 0
     for i := 0; i < readings_count; i++ {
       sum += array_of_arrays[j][i]
     }
 
-    if sum > (readings_count / 2) {
-      most_common_bits += "1"
-      least_common_bits += "0"
+    if sum >= (readings_count / 2) {
+      most_common_bits[j] = 1
+      least_common_bits[j] = 0
     } else {
-      most_common_bits += "0"
-      least_common_bits += "1"
+      most_common_bits[j] = 0
+      least_common_bits[j] = 1
     }
   }
 
-  gamma, err := strconv.ParseInt(most_common_bits, 2, 64)
-  epsilon, err := strconv.ParseInt(least_common_bits, 2, 64)
-  check(err)
+  return most_common_bits, least_common_bits
+}
 
-  fmt.Println(gamma * epsilon)
+func arrayToString(slice []int) string {
+  to_return := ""
+
+  for i := 0; i < len(slice); i++ {
+    to_return += strconv.Itoa(slice[i])
+  }
+
+  return to_return
 }
 
 func parseReading(bit string) int {
