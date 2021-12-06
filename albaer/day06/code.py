1692255c69eb0f5bc8cb0ac5b644dbf989eabe99
Annie Kiley <annie.kiley@viget.com>
2021-12-06 11:55:36 -0600

    Run part 1

--- albaer/day06/code.py
+++ albaer/day06/code.py
@@ -12,18 +12,46 @@ def read_input_lines(input_file_path="input.txt"):
 
 # Part 1
 
+def get_inputs_as_integers():
+  input_lines = read_input_lines()
+  first_line_as_str = input_lines[0]
+  str_inputs = first_line_as_str.split(",")
+  return [int(i) for i in str_inputs]
+
+def increment_day(lst):
+    # pprint(lst)
+    subtract_1_lst = [i - 1 for i in lst]
+    # pprint(subtract_1_lst)
+    count_reproducers = subtract_1_lst.count(-1)
+    # pprint(count_reproducers)
+    new_fish_lst = [8] * count_reproducers
+    # pprint(new_fish_lst)
+    reproduce_lst = subtract_1_lst + new_fish_lst
+    # pprint(reproduce_lst)
+    result = [6 if i == -1 else i for i in reproduce_lst]
+    # pprint(result)
+    return result
+
+def increment_days(lst, days, current_day=0):
+    if days == current_day:
+        return lst
+    else:
+        new_lst = increment_day(lst)
+        return increment_days(new_lst, days, current_day + 1)
+
 def represent_population(lst, days):
-    [i + days for i in lst]
+    return increment_days(lst, days)
 
 def count_population(lst, days):
-    len(represent_population(lst, days))
+    return len(represent_population(lst, days))
+
 # Part 2
 
 # Write solution
 
 if __name__ == '__main__':
-    # input_lines = read_input_lines()
-    part_1_result = "TODO"
+    input_ints = get_inputs_as_integers()
+    part_1_result = count_population(input_ints, 80)
     part_2_result = "TODO"
     solution = str(part_1_result) + "\n" + str(part_2_result)
     write_solution(solution)
--- albaer/day06/tests.py
+++ albaer/day06/tests.py
@@ -2,18 +2,18 @@ import unittest
 from code import *
 
 class TestDay06(unittest.TestCase):
-    EXAMPLE_INPUT = [3,4,3,1,2,]
+    EXAMPLE_INPUT = [3,4,3,1,2]
 
     # Part 1
 
     def test_represent_population_after_5_days(self):
         actual = represent_population(self.EXAMPLE_INPUT, 5)
-        expected = [5,6,5,3,4,5,6,7,7,8,]
+        expected = [5,6,5,3,4,5,6,7,7,8]
         self.assertEqual(actual, expected)
 
     def test_represent_population_after_18_days(self):
         actual = represent_population(self.EXAMPLE_INPUT, 18)
-        expected = [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
+        expected = [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8,]
         self.assertEqual(actual, expected)
 
     def test_count_population_after_18_days(self):
