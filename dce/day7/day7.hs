68b3fa0bd2a0ba18b994313b9a2ca7462dace0a6
David Eisinger <david.eisinger@gmail.com>
2021-12-07 13:07:53 -0500

    Use built-in 'minimum'/'maximum'

--- dce/day7/day7.hs
+++ dce/day7/day7.hs
@@ -5,24 +5,18 @@ buildList "" s = read s : []
 buildList (',' : cs) s = read s : buildList cs ""
 buildList (c : cs) s = buildList cs (s ++ [c])
 
-listMin :: [Int] -> Int
-listMin is = foldl min (is !! 0) is
-
-listMax :: [Int] -> Int
-listMax is = foldl max (is !! 0) is
-
 main :: IO ()
 main = do
     input <- readFile "./input.txt"
     let numbers = buildList input ""
-    let options = [(listMin numbers)..(listMax numbers)]
+    let options = [(minimum numbers)..(maximum numbers)]
 
     let cost newPos oldPos = abs (newPos - oldPos)
     let costs = map (\pos -> sum (map (cost pos) numbers)) options
 
-    putStrLn ("Part 1: " ++ (show (listMin costs)))
+    putStrLn ("Part 1: " ++ (show (minimum costs)))
 
     let cost newPos oldPos = sum [0..(abs (newPos - oldPos))]
     let costs = map (\pos -> sum (map (cost pos) numbers)) options
 
-    putStrLn ("Part 2: " ++ (show (listMin costs)))
+    putStrLn ("Part 2: " ++ (show (minimum costs)))
