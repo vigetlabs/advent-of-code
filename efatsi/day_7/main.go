6777d659ed0e455d657c2228f428a9df11afa784
Eli Fatsi <eli.fatsi@viget.com>
2021-12-07 12:57:03 -0500

    [ef] 7.2 solution, just kind of guessed and it worked :shrug:

--- efatsi/day_7/main.go
+++ efatsi/day_7/main.go
@@ -23,8 +23,22 @@ func main() {
   sort.Ints(positions)
   median := positions[len(positions) / 2]
   required_fuel := calculate_fuel(positions, median)
-  fmt.Println("Target: ", median)
-  fmt.Println("Fuel:   ", required_fuel)
+  fmt.Println("Fuel: ", required_fuel)
+
+  // Part 2
+  average := calculate_average(positions)
+  expensive_fuel := calculate_expensive_fuel(positions, int(average))
+  fmt.Println("Expensive Fuel: ", expensive_fuel)
+}
+
+func calculate_average(ints []int) float64 {
+  sum := 0
+
+  for _, x := range ints {
+    sum += x
+  }
+
+  return float64(sum) / float64(len(ints))
 }
 
 func calculate_fuel(positions []int, target int) int {
@@ -35,6 +49,22 @@ func calculate_fuel(positions []int, target int) int {
   }
 
   return fuel
+}
+
+func calculate_expensive_fuel(positions []int, target int) int {
+  fuel := 0
+
+  for _, x := range positions {
+    distance := int(math.Abs(float64(x - target)))
+    fuel += nth_triangle(distance)
+  }
+
+  return fuel
+}
+
+func nth_triangle(input int) int {
+  // input + (input - 1) + (input - 2) + ... + 0
+  return ((input * input) + input) / 2
 }
 
 func str_to_int(strings []string) []int {
