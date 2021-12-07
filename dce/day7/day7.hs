f99a946ba18ded900ab85a610da9b47fc2341e64
David Eisinger <david.eisinger@gmail.com>
2021-12-07 13:13:54 -0500

    Rename some vars

--- dce/day7/day7.hs
+++ dce/day7/day7.hs
@@ -8,15 +8,15 @@ buildList (c : cs) s = buildList cs (s ++ [c])
 main :: IO ()
 main = do
     input <- readFile "./input.txt"
-    let numbers = buildList input ""
-    let options = [(minimum numbers)..(maximum numbers)]
+    let positions = buildList input ""
+    let options = [(minimum positions)..(maximum positions)]
 
-    let cost newPos oldPos = abs (newPos - oldPos)
-    let costs = map (\pos -> sum (map (cost pos) numbers)) options
+    let cost pos1 pos2 = abs (pos1 - pos2)
+    let costs = map (\p -> sum (map (cost p) positions)) options
 
     putStrLn ("Part 1: " ++ (show (minimum costs)))
 
-    let cost newPos oldPos = sum [0..(abs (newPos - oldPos))]
-    let costs = map (\pos -> sum (map (cost pos) numbers)) options
+    let cost pos1 pos2 = sum [0..(abs (pos1 - pos2))]
+    let costs = map (\p -> sum (map (cost p) positions)) options
 
     putStrLn ("Part 2: " ++ (show (minimum costs)))
