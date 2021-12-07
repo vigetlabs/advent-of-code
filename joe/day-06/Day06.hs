5755f10bc2991c98fd7f4034dd75ef35a1bdbda8
Joe Jackson <cpmhjoe@gmail.com>
2021-12-07 09:51:15 -0500

    Clean Up

--- joe/day-06/Day06.hs
+++ joe/day-06/Day06.hs
@@ -11,21 +11,10 @@ splitBy c s = case dropWhile (== c) s of
 exampleData :: String
 exampleData = "3,4,3,1,2"
 
-newtype LanternFish = LanternFish Int deriving (Show, Eq)
 data Counts a = Counts a a a a a a a a a deriving (Show, Foldable)
 
-tick :: LanternFish -> [LanternFish]
-tick (LanternFish 0) = [LanternFish 6, LanternFish 8]
-tick (LanternFish n) = [LanternFish $ n - 1]
-
-prepare :: String -> [LanternFish]
-prepare s = LanternFish . read <$> splitBy ',' s
-
-solve :: [LanternFish] -> [[LanternFish]]
-solve fishes = fishes : solve (concatMap tick fishes)
-
-prepare2 :: String -> Counts Int
-prepare2 s = let sortedInputs = sort $ read <$> splitBy ',' s :: [Int] in
+prepare :: String -> Counts Int
+prepare s = let sortedInputs = sort $ read <$> splitBy ',' s :: [Int] in
   Counts (length $ filter (== 0) sortedInputs)
          (length $ filter (== 1) sortedInputs)
          (length $ filter (== 2) sortedInputs)
@@ -36,16 +25,15 @@ prepare2 s = let sortedInputs = sort $ read <$> splitBy ',' s :: [Int] in
          (length $ filter (== 7) sortedInputs)
          (length $ filter (== 8) sortedInputs)
 
-solve2 :: Counts Int -> [Counts Int]
-solve2 cs@(Counts a b c d e f g h i) = cs : solve2 (Counts b c d e f g (h+a) i a)
+solve :: Counts Int -> [Counts Int]
+solve cs@(Counts a b c d e f g h i) = cs : solve (Counts b c d e f g (h+a) i a)
 
 main :: IO ()
 main = do
   input <-  readFile "day-06/input.txt"
-  let fishes = prepare input
+  let counts = prepare input
   print "Day 1"
-  print $ length $ last $ take 81 $ solve fishes
+  print $ sum $ (!! 80) $ solve counts
 
-  let counts = prepare2 input
   print "Day 2"
-  print $ sum $ last $ take 257 $ solve2 counts
+  print $ sum $ (!! 256) $ solve counts
