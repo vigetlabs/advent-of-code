bc514ca8ecf89d41219379b76f4dcc6cda0e0ba1
Joe Jackson <cpmhjoe@gmail.com>
2021-12-08 12:43:16 -0500

    Day 8 -- Part 1

---
+++ joe/day-08/Day08.hs
@@ -0,0 +1,42 @@
+module Day08 where
+
+-- My setup is strange and I'm not happy about it. For now just copying
+splitBy :: Char -> String -> [String]
+splitBy c s = case dropWhile (== c) s of
+  "" -> []
+  s' -> w : splitBy c s'' where (w, s'') = break (== c) s'
+
+exampleData :: String
+exampleData = unlines [
+    "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+  , "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc"
+  , "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg"
+  , "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb"
+  , "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea"
+  , "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb"
+  , "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe"
+  , "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef"
+  , "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb"
+  , "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"]
+
+data Signal a = Signal [a] [a] deriving Show
+
+prepare :: String -> [Signal String]
+prepare input = toSignal . map words . splitBy '|' <$> lines input
+  where
+    toSignal [x,y] = Signal x y
+    toSignal _ = error "signal must have input and output"
+
+solve1 :: [Signal String] -> Int
+solve1 = sum . map countDigits
+  where
+    countDigits (Signal _ xs) = length
+      $ filter (`elem` [2,3,4,7])
+      $ length <$> xs
+
+main :: IO ()
+main = do
+  input <- readFile "day-08/input.txt"
+  let signals = prepare input
+
+  print $ solve1 signals
