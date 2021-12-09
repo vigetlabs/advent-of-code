92807bec78355201e5e9e015da69ed6f4ba5ba11
Annie Kiley <annie.kiley@viget.com>
2021-12-08 21:02:08 -0600

    Working solution for part 1

--- albaer/day08/code.py
+++ albaer/day08/code.py
@@ -17,14 +17,38 @@ def split_inputs_from_first_line():
 
 # Part 1
 
-def count_1_4_7_8(inputs):
-    return 0
+def get_entries(entries_lst):
+    split_sps_ovs = [entry.split(" | ") for entry in entries_lst]
+    return [{"sps": i[0].split(" "), "ovs": i[1].split(" ")} for i in split_sps_ovs]
 
+def get_output_values(entries_lst):
+    entries = get_entries(entries_lst)
+    return [i["ovs"] for i in entries]
+
+def count_1s(digits_lst):
+    return sum(len(i) == 2 for i in digits_lst)
+
+def count_4s(digits_lst):
+    return sum(len(i) == 4 for i in digits_lst)
+
+def count_7s(digits_lst):
+    return sum(len(i) == 3 for i in digits_lst)
+
+def count_8s(digits_lst):
+    return sum(len(i) == 7 for i in digits_lst)
+
+def count_1_4_7_8_digits(digits_lst):
+    return sum([count_1s(digits_lst), count_4s(digits_lst), count_7s(digits_lst), count_8s(digits_lst)])
+
+def count_1_4_7_8(entries_lst):
+    output_values = get_output_values(entries_lst)
+    return sum([count_1_4_7_8_digits(digits_lst) for digits_lst in output_values])
 
 # Write solution
 
 if __name__ == '__main__':
-    part_1_result = "TODO"
+    entries = read_input_lines()
+    part_1_result = count_1_4_7_8(entries)
     part_2_result = "TODO"
     solution = str(part_1_result) + "\n" + str(part_2_result)
     write_solution(solution)
