1fc5dea5978a6e9073961aced025a900aa1431fe
Eli Fatsi <eli.fatsi@viget.com>
2021-12-10 06:58:30 -0500

    [ef] refactor with Position struct and neighbors method

--- efatsi/day_9/main.go
+++ efatsi/day_9/main.go
@@ -15,6 +15,12 @@ const filename = "example.txt"
 // const height = 100
 // const filename = "input.txt"
 
+type Position struct {
+  x int
+  y int
+  value int
+}
+
 func main() {
   data, _ := os.ReadFile(filename)
 
@@ -23,13 +29,13 @@ func main() {
 
   heightmap := loadHeightmap(ventLines)
 
-  lowPoints := make([]int, 0)
+  lowPoints := make([]Position, 0)
   for y := 0; y < height; y++ {
     for x := 0; x < width; x++ {
-      reading := heightmap[y][x]
+      position := heightmap[y][x]
 
-      if isLowpoint(heightmap, reading, x, y) {
-        lowPoints = append(lowPoints, reading)
+      if isLowpoint(heightmap, position) {
+        lowPoints = append(lowPoints, position)
       }
     }
   }
@@ -37,50 +43,67 @@ func main() {
   solvePartOne(lowPoints)
 }
 
-func solvePartOne(lowPoints []int) {
+func solvePartOne(lowPoints []Position) {
   riskLevel := 0
   for _, point := range lowPoints {
-    riskLevel += point + 1
+    riskLevel += point.value + 1
   }
   fmt.Println("Risk Level:", riskLevel)
 }
 
-func isLowpoint(heightmap [height][width]int, reading int, x int, y int) bool {
-  // Check Up
-  if y > 0 && heightmap[y-1][x] <= reading {
-    return false
-  }
-
-  // Check Down
-  if y < height - 1 && heightmap[y+1][x] <= reading {
-    return false
-  }
-
-  // Check Left
-  if x > 0 && heightmap[y][x-1] <= reading {
-    return false
-  }
-
-  // Check Right
-  if x < width - 1 && heightmap[y][x+1] <= reading {
-    return false
+func isLowpoint(heightmap [height][width]Position, position Position) bool {
+  for _, neighbor := range position.neighbors(heightmap) {
+    if neighbor.value <= position.value {
+      // Found a neighbor who's equal or lower, return false
+      return false
+    }
   }
 
   // Else, all adjascent values are higher, we have a low point!
   return true
 }
 
-func loadHeightmap(ventLines []string) [height][width]int {
-  var heightmap [height][width]int
+func loadHeightmap(ventLines []string) [height][width]Position {
+  var heightmap [height][width]Position
 
   for y, line := range ventLines {
     ventReadings := strings.Split(line, "")
 
     for x, reading := range ventReadings {
       val, _ := strconv.Atoi(reading)
-      heightmap[y][x] = val
+      heightmap[y][x] = Position{x, y, val}
     }
   }
 
   return heightmap
+}
+
+
+func (position *Position) neighbors(heightmap [height][width]Position) []Position {
+  neighbors := make([]Position, 0)
+
+  x := position.x
+  y := position.y
+
+  // Check Up
+  if y > 0 {
+    neighbors = append(neighbors, heightmap[y-1][x])
+  }
+
+  // Check Down
+  if y < height - 1 {
+    neighbors = append(neighbors, heightmap[y+1][x])
+  }
+
+  // Check Left
+  if x > 0 {
+    neighbors = append(neighbors, heightmap[y][x-1])
+  }
+
+  // Check Right
+  if x < width - 1 {
+    neighbors = append(neighbors, heightmap[y][x+1])
+  }
+
+  return neighbors
 }
