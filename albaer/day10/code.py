87bb9cfd8987bd76635a03bab07f5b91c9eeacda
Annie Kiley <annie.kiley@viget.com>
2021-12-10 21:25:10 -0600

    Working solution for part 2

--- albaer/day10/code.py
+++ albaer/day10/code.py
@@ -32,10 +32,15 @@ POINTS = {
     ">": 25137,
 }
 
-def calculate_points(line):
-    pprint(line)
+COMPLETION_POINTS = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+def calculate_syntax_points(line):
     if line == "":
-        pprint("valid")
         return 0
 
     pattern = r"([\{\[\(\<])([\}\]\)\>])"
@@ -48,23 +53,62 @@ def calculate_points(line):
         valid_close_char = VALID_PAIRS[open_char]
 
         if close_char == valid_close_char:
-            pprint(f"removing {pair}")
-            return calculate_points(line.replace(pair, ""))
+            return calculate_syntax_points(line.replace(pair, ""))
         else:
-            pprint(f"invalid: expected {valid_close_char} but got {close_char} instead")
             return POINTS[close_char]
     else:
-        pprint("incomplete")
         return 0
 
-def total_points(lines):
-    return sum([calculate_points(i) for i in lines])
+def total_syntax_points(lines):
+    return sum([calculate_syntax_points(i) for i in lines])
+# Part 2
+
+def find_completion_string(line):
+    if line == "":
+        return ""
+
+    pattern = r"([\{\[\(\<])([\}\]\)\>])"
+    m = re.search(pattern, line)
+
+    if m:
+        pair = m.group(0)
+        open_char = m.group(1)
+        close_char = m.group(2)
+        valid_close_char = VALID_PAIRS[open_char]
+
+        if close_char == valid_close_char:
+            return find_completion_string(line.replace(pair, ""))
+        else:
+            return ""
+    else:
+        open_chars = list(line)
+        close_chars = [VALID_PAIRS[i] for i in open_chars][::-1]
+        completion_string = "".join(close_chars)
+        return completion_string
+
+def score_completion_string(completion_string, score=0):
+    if completion_string == "":
+        return score
+    else:
+        new_score = (score * 5) + COMPLETION_POINTS[completion_string[0]]
+        new_string = completion_string[1:]
+        return score_completion_string(new_string, new_score)
+
+def score_completion_line(line):
+    completion_string = find_completion_string(line)
+    return score_completion_string(completion_string)
+
+def score_completion_lines(lst):
+    completion_scores = [score_completion_line(i) for i in lst]
+    sorted_scores = sorted([i for i in completion_scores if i != 0])
+    middle_index = int((len(sorted_scores) - 1)/2)
+    return sorted_scores[middle_index]
 
 # Write solution
 
 if __name__ == '__main__':
     inputs = read_input_lines()
-    part_1_result = total_points(inputs)
-    part_2_result = "TODO"
+    part_1_result = total_syntax_points(inputs)
+    part_2_result = score_completion_lines(inputs)
     solution = str(part_1_result) + "\n" + str(part_2_result)
     write_solution(solution)
--- albaer/day10/tests.py
+++ albaer/day10/tests.py
@@ -14,44 +14,62 @@ class TestDay10(unittest.TestCase):
       "<{([([[(<>()){}]>(<<{{",
       "<{([{{}}[<[[[<>{}]]]>[]]",
     ]
+
     EXAMPLE_CORRUPT_BRACE = "{([(<{}[<>[]}>{[]{[(<()>" # Expected ], but found } instead.
     EXAMPLE_CORRUPT_PAREN = "[[<[([]))<([[{}[[()]]]" # Expected ], but found ) instead.
     EXAMPLE_CORRUPT_BRACK = "[{[{({}]{}}([{[{{{}}([]" # Expected ), but found ] instead.
     EXAMPLE_CORRUPT_PAREN_2 = "[<(<(<(<{}))><([]([]()" # Expected >, but found ) instead.
     EXAMPLE_CORRUPT_ARROW = "<{([([[(<>()){}]>(<<{{" # Expected ], but found > instea
+    EXAMPLE_INCOMPLETE = "<{([{{}}[<[[[<>{}]]]>[]]"
 
     # Part 1
 
-    def test_calculate_points_0(self):
-        actual = calculate_points(self.EXAMPLE_CORRUPT_BRACE)
+    def test_calculate_syntax_points_0(self):
+        actual = calculate_syntax_points(self.EXAMPLE_CORRUPT_BRACE)
         expected = 1197
         self.assertEqual(actual, expected)
 
-    def test_calculate_points_1(self):
-        actual = calculate_points(self.EXAMPLE_CORRUPT_PAREN)
+    def test_calculate_syntax_points_1(self):
+        actual = calculate_syntax_points(self.EXAMPLE_CORRUPT_PAREN)
         expected = 3
         self.assertEqual(actual, expected)
 
-    def test_calculate_points_2(self):
-        actual = calculate_points(self.EXAMPLE_CORRUPT_BRACK)
+    def test_calculate_syntax_points_2(self):
+        actual = calculate_syntax_points(self.EXAMPLE_CORRUPT_BRACK)
         expected = 57
         self.assertEqual(actual, expected)
 
-    def test_calculate_points_3(self):
-        actual = calculate_points(self.EXAMPLE_CORRUPT_PAREN_2)
+    def test_calculate_syntax_points_3(self):
+        actual = calculate_syntax_points(self.EXAMPLE_CORRUPT_PAREN_2)
         expected = 3
         self.assertEqual(actual, expected)
 
-    def test_calculate_points_4(self):
-        actual = calculate_points(self.EXAMPLE_CORRUPT_ARROW)
+    def test_calculate_syntax_points_4(self):
+        actual = calculate_syntax_points(self.EXAMPLE_CORRUPT_ARROW)
         expected = 25137
         self.assertEqual(actual, expected)
 
-    def test_total_points(self):
-        actual = total_points(self.EXAMPLE_INPUT)
+    def test_total_syntax_points(self):
+        actual = total_syntax_points(self.EXAMPLE_INPUT)
         expected = 26397
         self.assertEqual(actual, expected)
 
+# Part 2
+
+    def test_find_completion_string(self):
+        actual = find_completion_string(self.EXAMPLE_INCOMPLETE)
+        expected = "])}>"
+        self.assertEqual(actual, expected)
+
+    def test_score_completion_line(self):
+        actual = score_completion_line(self.EXAMPLE_INCOMPLETE)
+        expected = 294
+        self.assertEqual(actual, expected)
+
+    def test_score_completion_lines(self):
+        actual = score_completion_lines(self.EXAMPLE_INPUT)
+        expected = 288957
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
