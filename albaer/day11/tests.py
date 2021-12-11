d05470e0a98734d823d2c045a50e48cb70c52777
Annie Kiley <annie.kiley@viget.com>
2021-12-11 13:22:27 -0600

    Set up day 11

---
+++ albaer/day11/tests.py
@@ -0,0 +1,18 @@
+import unittest
+from code import *
+
+class TestDay11(unittest.TestCase):
+    EXAMPLE_INPUT = []
+
+    # Part 1
+
+    def test_some_method(self):
+        actual = some_method(self.EXAMPLE_INPUT)
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    # Part 2
+
+if __name__ == '__main__':
+    unittest.main()
+
