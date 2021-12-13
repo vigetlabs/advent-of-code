f73d956912ba2a8007c651274fa1ad90fb3a079a
Solomon Hawk <solomon.hawk@viget.com>
2021-12-13 12:01:43 -0500

    [sfh] Order mods in lib.rs

--- solomonhawk/src/lib.rs
+++ solomonhawk/src/lib.rs
@@ -4,7 +4,6 @@ extern crate aoc_runner;
 extern crate aoc_runner_derive;
 
 pub mod day1;
-pub mod day11;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -14,5 +13,6 @@ pub mod day7;
 pub mod day8;
 pub mod day9;
 pub mod day10;
+pub mod day11;
 
 aoc_lib! { year = 2021 }
