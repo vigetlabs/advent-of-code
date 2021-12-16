b2b7ba29458a1ee0a1a2402ee614b459b912a62f
Solomon Hawk <solomon.hawk@viget.com>
2021-12-15 21:31:38 -0500

    [sfh] Day 15, Part 2

--- solomonhawk/src/day15.rs
+++ solomonhawk/src/day15.rs
@@ -2,13 +2,13 @@
 use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 
-type Point = (i8, i8);
+type Point = (isize, isize);
 
 const OFFSETS: [Point; 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
 
 #[derive(Debug)]
 pub struct Edge {
-    node: usize, // index in node list
+    node: usize, // index in node list this edge points to
     cost: usize, // cost to move from the node that owns this edge to the node at `node`
 }
 
@@ -18,46 +18,7 @@ pub struct Node {
     edges: Vec<Edge>,
 }
 
-#[aoc_generator(day15)]
-pub fn input_generator(input: &str) -> Vec<Node> {
-    let points: Vec<Vec<usize>> = input
-        .split("\n")
-        .map(|row| row.chars().map(|n| n as usize - '0' as usize).collect())
-        .collect();
-
-    points
-        .iter()
-        .enumerate()
-        .flat_map(|(y, row)| {
-            let points = points.clone();
-
-            (0..row.len()).map(move |x| {
-                let position = (x as i8, y as i8);
-
-                Node {
-                    position,
-                    edges: find_edges(&points, position),
-                }
-            })
-        })
-        .collect()
-}
-
-#[aoc(day15, part1)]
-pub fn part1(graph: &Vec<Node>) -> usize {
-    if let Some(distance) = shortest_path(&graph, 0, graph.len() - 1) {
-        distance
-    } else {
-        panic!("Could not find a shortest path")
-    }
-}
-
-#[aoc(day15, part2)]
-pub fn part2(graph: &Vec<Node>) -> usize {
-    0
-}
-
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 struct State {
     cost: usize,
     position: usize,
@@ -79,22 +40,109 @@ impl PartialOrd for State {
     }
 }
 
+#[aoc_generator(day15, part1)]
+pub fn input_generator_part1(input: &str) -> Vec<Node> {
+    let points: Vec<Vec<usize>> = generate_points(input);
+    generate_nodes(points)
+}
+
+#[aoc_generator(day15, part2)]
+pub fn input_generator_part2(input: &str) -> Vec<Node> {
+    let mut points: Vec<Vec<usize>> = generate_points(input);
+    expand_points(&mut points);
+    generate_nodes(points)
+}
+
+// string -> 2D Vec of integer (risk values)
+fn generate_points(input: &str) -> Vec<Vec<usize>> {
+    input
+        .split("\n")
+        .map(|row| row.chars().map(|n| n as usize - '0' as usize).collect())
+        .collect()
+}
+
+// 2D Vec of integers -> Vec of Node
+fn generate_nodes(points: Vec<Vec<usize>>) -> Vec<Node> {
+    points
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            let points = points.clone();
+
+            (0..row.len()).map(move |x| {
+                let position = (x as isize, y as isize);
+
+                Node {
+                    position,
+                    edges: find_edges(&points, position),
+                }
+            })
+        })
+        .collect()
+}
+
+// 🤷🏻‍♂️ tile the points 5 times horizontally and vertically, this is gross
+fn expand_points(points: &mut Vec<Vec<usize>>) {
+    let original_points = points.clone();
+    let height = original_points.len();
+    let width = original_points[0].len();
+
+    // expand horizontally
+    for dx in 0..4 {
+        for y in 0..height {
+            for x in 0..width {
+                let value = points[y][x + width * dx];
+                points[y].push(value % 9 + 1);
+            }
+        }
+    }
+
+    // expand vertically
+    for dy in 1..5 {
+        for y in 0..height {
+            points.push(Vec::with_capacity(width * 5));
+
+            for x in 0..width * 5 {
+                let new_y = y + height * dy;
+                let value = points[new_y - height][x];
+                points[new_y].push(value % 9 + 1);
+            }
+        }
+    }
+}
+
+#[aoc(day15, part1)]
+pub fn part1(graph: &Vec<Node>) -> usize {
+    if let Some(distance) = shortest_path(&graph, 0, graph.len() - 1) {
+        distance
+    } else {
+        panic!("Could not find a shortest path")
+    }
+}
+
+#[aoc(day15, part2)]
+pub fn part2(graph: &Vec<Node>) -> usize {
+    if let Some(distance) = shortest_path(&graph, 0, graph.len() - 1) {
+        distance
+    } else {
+        panic!("Could not find a shortest path")
+    }
+}
+
 fn find_edges(points: &Vec<Vec<usize>>, origin: Point) -> Vec<Edge> {
-    valid_neighbors(&origin, (points[0].len(), points.len()))
+    valid_neighbors(&origin, (points[0].len() as isize, points.len() as isize))
         .iter()
         .map(|&(x, y)| {
             Edge {
-                node: x as usize + y as usize * points.len(), // dest node position in master list
+                // dest node position in master list
+                node: x as usize + y as usize * points.len(),
                 cost: points[y as usize][x as usize],
             }
         })
         .collect()
 }
 
-// A* https://en.wikipedia.org/wiki/A*_search_algorithm
-// Manhattan Distance https://en.wikipedia.org/wiki/Taxicab_geometry
 // Djikstra's Algorithm https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-// Binary Heap https://doc.rust-lang.org/std/collections/binary_heap/index.html
 fn shortest_path(graph: &Vec<Node>, start: usize, end: usize) -> Option<usize> {
     let mut dist: Vec<_> = (0..graph.len()).map(|_| usize::MAX).collect();
     let mut heap = BinaryHeap::new();
@@ -107,7 +155,7 @@ fn shortest_path(graph: &Vec<Node>, start: usize, end: usize) -> Option<usize> {
         position: start,
     });
 
-    // iterate over the lowest cost next nodes
+    // iterate over the lowest cost next node
     while let Some(State { cost, position }) = heap.pop() {
         if position == end {
             return Some(cost);
@@ -138,13 +186,13 @@ fn shortest_path(graph: &Vec<Node>, start: usize, end: usize) -> Option<usize> {
     None
 }
 
-fn valid_neighbors(point: &Point, dimensions: (usize, usize)) -> Vec<Point> {
+fn valid_neighbors(point: &Point, dimensions: (isize, isize)) -> Vec<Point> {
     OFFSETS
         .iter()
         .map(|(dx, dy)| (point.0 + dx, point.1 + dy))
         .filter(|&(x, y)| valid_point(x, y, dimensions.0, dimensions.1))
         .collect()
 }
-fn valid_point(x: i8, y: i8, width: usize, height: usize) -> bool {
-    x >= 0 && (x as usize) < width && y >= 0 && (y as usize) < height
+fn valid_point(x: isize, y: isize, width: isize, height: isize) -> bool {
+    x >= 0 && x < width && y >= 0 && y < height
 }
