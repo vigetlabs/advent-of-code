cb0060d3bb8f06288d8712f8294eccb6764411ba
Annie Kiley <annie.kiley@viget.com>
2021-12-16 21:41:17 -0600

    Working solution for part 1

--- albaer/day12/code.py
+++ albaer/day12/code.py
@@ -17,9 +17,55 @@ def split_inputs_from_first_line():
 
 # Part 1
 
-def some_method(_):
-    return 0
+# def connected_caves(tunnel_lst):
+#     return [conn.split("-") for conn in tunnel_lst]
 
+
+# def caves_from(tunnel_lst, cave):
+#     return [cave for cave in tunnel_lst if ]
+
+# Utilities
+
+def flatten(lst):
+    return [item for sublist in lst for item in sublist]
+
+# Part 1
+
+def caves_connected_to(tunnels, cave_a):
+    tunnels = [tunnel for tunnel in tunnels if cave_a in tunnel]
+    return [tunnel.replace(cave_a, "").replace("-", "") for tunnel in tunnels]
+
+def add_cave_to_path_list(path_list, cave):
+    new_path_list = path_list.copy()
+    new_path_list.append(cave)
+    return new_path_list
+
+def paths_from(tunnels, path_list):
+    last_cave = path_list[-1]
+    possible_next_caves = caves_connected_to(tunnels, last_cave)
+    valid_next_caves = [cave for cave in possible_next_caves if valid_next_cave(path_list, cave)]
+    result = [add_cave_to_path_list(path_list, cave) for cave in valid_next_caves]
+    return result
+
+def valid_next_cave(path_list, cave):
+    if cave == "start":
+        return False
+    elif cave.islower() and cave in path_list:
+        return False
+    else:
+        return True
+
+def find_paths(tunnels, paths=[["start"]], complete_paths=[]):
+    if paths == []:
+        return complete_paths
+    else:
+        all_paths = flatten([paths_from(tunnels, path) for path in paths])
+        new_complete_paths = complete_paths + [path for path in all_paths if path[-1] == "end"]
+        new_paths = [path for path in all_paths if path[-1] != "end"]
+        return find_paths(tunnels, new_paths, new_complete_paths)
+
+def count_distinct_paths(tunnels):
+    return len(find_paths(tunnels))
 
 # Write solution
 
--- albaer/day12/tests.py
+++ albaer/day12/tests.py
@@ -2,13 +2,73 @@ import unittest
 from code import *
 
 class TestDay12(unittest.TestCase):
-    EXAMPLE_INPUT = []
+    EXAMPLE_INPUT_A = [
+        "start-A",
+        "start-b",
+        "A-c",
+        "A-b",
+        "b-d",
+        "A-end",
+        "b-end",
+    ]
+
+    EXAMPLE_INPUT_B = [
+        "dc-end",
+        "HN-start",
+        "start-kj",
+        "dc-start",
+        "dc-HN",
+        "LN-dc",
+        "HN-end",
+        "kj-sa",
+        "kj-HN",
+        "kj-dc",
+    ]
+
+    EXAMPLE_INPUT_C = [
+        "fs-end",
+        "he-DX",
+        "fs-he",
+        "start-DX",
+        "pj-DX",
+        "end-zg",
+        "zg-sl",
+        "zg-pj",
+        "pj-he",
+        "RW-he",
+        "fs-DX",
+        "pj-RW",
+        "zg-RW",
+        "start-pj",
+        "he-WI",
+        "zg-he",
+        "pj-fs",
+        "start-RW",
+    ]
 
     # Part 1
 
-    def test_some_method(self):
-        actual = some_method(self.EXAMPLE_INPUT)
-        expected = 0
+    # caves_connected_to(tunnels, cave):
+    # def test_caves_connected_to_a(self):
+    #     actual = caves_connected_to(self.EXAMPLE_INPUT_A, "start")
+    #     expected = ["A", "b"]
+    #     self.assertEqual(actual, expected)
+
+    # count_distinct_paths(tunnels)
+
+    def test_count_distinct_paths_a(self):
+        actual = count_distinct_paths(self.EXAMPLE_INPUT_A)
+        expected = 10
+        self.assertEqual(actual, expected)
+
+    def test_count_distinct_paths_b(self):
+        actual = count_distinct_paths(self.EXAMPLE_INPUT_B)
+        expected = 19
+        self.assertEqual(actual, expected)
+
+    def test_count_distinct_paths_c(self):
+        actual = count_distinct_paths(self.EXAMPLE_INPUT_C)
+        expected = 226
         self.assertEqual(actual, expected)
 
     # Part 2
