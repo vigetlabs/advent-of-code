b1cee6f901ddebfc1b406b0dff9a8ea1e57e8e46
Eli Fatsi <eli.fatsi@viget.com>
2021-12-17 08:52:35 -0500

    [ef] Add test for packet value determining, and giving up

--- efatsi/day_16/main_test.go
+++ efatsi/day_16/main_test.go
@@ -28,8 +28,8 @@ func TestReadLiteralPacket(t *testing.T) {
     expected := expecteds[i]
     packet := readLiteralPacket(input)
 
-    if (packet.value != expected) {
-      t.Errorf("Fail! Expected %d, got %d", expected, packet.value)
+    if (packet.rawValue != expected) {
+      t.Errorf("Fail! Expected %d, got %d", expected, packet.rawValue)
     }
   }
 }
@@ -49,5 +49,28 @@ func TestReadOperatorPacket1(t *testing.T) {
 
   if (len(packet.subPackets) != 3) {
     t.Errorf("Fail! Expected %d, got %d", 3, len(packet.subPackets))
+  }
+}
+
+func TestPacketValue(t *testing.T) {
+  inputOutputs := map[string]int{
+    "C200B40A82": 3,
+    "04005AC33890": 54,
+    "880086C3E88112": 7,
+    "CE00C43D881120": 9,
+    "D8005AC2A8F0": 1,
+    "F600BC2D8F": 0,
+    "9C005AC2F8F0": 0,
+    "9C0141080250320F1802104A08": 1,
+  }
+
+  for input, output := range inputOutputs {
+    binary := hexToBin(input)
+    packet := readPacket(binary)
+    got := packet.value()
+
+    if got != output {
+      t.Errorf("Fail! Expected %d, got %d", output, got)
+    }
   }
 }
