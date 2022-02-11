4990eb5e9ed55db60f31eb118940aeca4aaabc2f
Noah Over <noahko96@gmail.com>
2022-02-11 15:46:02 -0500

    Copy over Day 1 solution

--- noahover/day1/lib/day1.ex
+++ noahover/day1/lib/day1.ex
@@ -1,18 +1,40 @@
 defmodule Day1 do
   @moduledoc """
-  Documentation for `Day1`.
+  Advent of Code 2021 Day 1
   """
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x -> String.to_integer(x) end)
+  end
 
   @doc """
-  Hello world.
+  Day 1 Part 1
 
-  ## Examples
-
-      iex> Day1.hello()
-      :world
-
+  Find number of times depth measurement increase
   """
-  def hello do
-    :world
+  def count_increases([]), do: 0
+  def count_increases([_val]), do: 0
+  def count_increases([head | [tail_head | _tail_tail] = tail]) do
+    cond do
+      tail_head > head -> 1 + count_increases(tail)
+      true -> count_increases(tail)
+    end
+  end
+
+  @doc """
+  Day 1 Part 2
+
+  Find number of times sum of three adjacent depth measurements increases
+  """
+  def count_sum_increases(list) when length(list) < 4, do: 0
+  def count_sum_increases([first | [second | [third | [fourth | _fourth_tail]]] = tail]) do
+    first_sum = first + second + third
+    second_sum = second + third + fourth
+    cond do
+      second_sum > first_sum -> 1 + count_sum_increases(tail)
+      true -> count_sum_increases(tail)
+    end
   end
 end
--- noahover/day1/test/day1_test.exs
+++ noahover/day1/test/day1_test.exs
@@ -2,7 +2,34 @@ defmodule Day1Test do
   use ExUnit.Case
   doctest Day1
 
-  test "greets the world" do
-    assert Day1.hello() == :world
+  import Day1
+
+  @example_input """
+  199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  """
+
+  test "day 1 part 1 example - counts increases" do
+    answer =
+      @example_input
+      |> parse_input()
+      |> count_increases()
+    assert answer == 7
+  end
+
+  test "day 1 part 2 example - counts sum increases" do
+    answer =
+      @example_input
+      |> parse_input()
+      |> count_sum_increases()
+    assert answer == 5
   end
 end
