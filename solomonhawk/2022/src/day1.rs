c91216e5c6ec404797e17c35041209aadcbbaf2a
Solomon Hawk <solomon.hawk@viget.com>
2022-12-01 15:27:57 -0500

    [sfh] remove directive allowing unused variables

--- solomonhawk/2022/src/day1.rs
+++ solomonhawk/2022/src/day1.rs
@@ -1,5 +1,3 @@
-#![allow(unused_variables)]
-
 /*
 --- Day 1: Calorie Counting ---
 Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
