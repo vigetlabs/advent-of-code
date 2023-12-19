5fa6263c32546ad3d92f50422ae95ea830e49d8e
Noah Over <noahko96@gmail.com>
2023-12-19 13:11:01 -0500

    2023 Day 1 Part 2

--- noahover/aoc23/test/day1_test.exs
+++ noahover/aoc23/test/day1_test.exs
@@ -4,9 +4,9 @@ defmodule Day1Test do
 
   import Day1
 
-  test "day 1 part 1 example - sum calibration values" do
+  test "day 1 part 1 example 1 - sum calibration values" do
     answer =
-      File.read!("input/day1_example.txt")
+      File.read!("input/day1_example1.txt")
       |> parse_input()
       |> sum_calibration_values()
 
@@ -20,5 +20,23 @@ defmodule Day1Test do
       |> sum_calibration_values()
 
     assert answer == 54450
+  end
+
+  test "day 1 part 2 example 2 - sum calibration values with words" do
+    answer =
+      File.read!("input/day1_example2.txt")
+      |> parse_input()
+      |> sum_calibration_values_with_words()
+
+    assert answer == 281
+  end
+
+  test "day 1 part 2 - sum calibration values with words" do
+    answer =
+      File.read!("input/day1.txt")
+      |> parse_input()
+      |> sum_calibration_values_with_words()
+
+    assert answer == 54265
   end
 end
