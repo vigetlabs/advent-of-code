d9db16dcf841fbd0f97e1fe2698eb0d920603645
Noah Over <noahko96@gmail.com>
2023-12-20 14:45:26 -0500

    2023 Day 2 Part 2

--- noahover/aoc23/test/day2_test.exs
+++ noahover/aoc23/test/day2_test.exs
@@ -21,4 +21,22 @@ defmodule Day2Test do
 
     assert answer == 2278
   end
+
+  test "day 2 part 2 example 1 - sum game powers" do
+    answer =
+      File.read!("input/day2_example.txt")
+      |> parse_input()
+      |> sum_game_powers()
+
+    assert answer == 2286
+  end
+
+  test "day 2 part 2 - sum game powers" do
+    answer =
+      File.read!("input/day2.txt")
+      |> parse_input()
+      |> sum_game_powers()
+
+    assert answer == 67953
+  end
 end
